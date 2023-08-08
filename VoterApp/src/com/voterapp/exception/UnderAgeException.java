abca83e9f5421bb04962b284bd0e753c582f07ee
DarshanaRajendran <103446653+DarshanaRajendran@users.noreply.github.com>
2023-08-08 16:41:32 +0530

    Add files via upload

---
+++ VoterApp/src/com/voterapp/exception/UnderAgeException.java
@@ -0,0 +1,12 @@
+package com.voterapp.exception;
+
+public class UnderAgeException extends Exception{
+	public UnderAgeException() {
+		super();
+	}
+
+	public UnderAgeException(String message) {
+		super(message);
+	}
+
+}
